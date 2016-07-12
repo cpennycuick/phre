dea9da428c7cf99d1a9af4f4c7648ae6ce8115d5
Chris Pennycuick <christopher.pennycuick@gmail.com>
2016-07-12 23:54:46 +1000

    Add report and config documentation

--- src/Report.php
+++ src/Report.php
@@ -5,6 +5,35 @@ namespace PHRE;
 use PHRE\DataSource\DataSource;
 use PHRE\DataHolder\StyleSheet;
 
+/**
+ * Class for defining a report.
+ *
+ * This class is the first entry point from which the report
+ * structure is defined. All other elements are added to this
+ * class.
+ *
+ * Currently this report engine only outputs in two different
+ * formats: HTML and PDF. In both cases, the string contents
+ * is returned to the user to handle. The user can save it to
+ * a file, or output it to the client.
+ *
+ * When outputting PDF, use <code>header('Content-Type: application/pdf');</code>
+ * before echoing the contents to the client. The page will now
+ * render as a PDF.
+ *
+ * <b>Basic example:</b>
+ * <pre>
+ * PHRE\Report::create()
+ *     ->add(PHRE\Entities\Tag::h1()
+ *         ->add(PHRE\Entities\Text::create('Hello World!')
+ *     )
+ *     ->generateHTML($data);
+ * </pre>
+ * <b>Output:</b>
+ * <pre>
+ * &lt;h1&gt;Hello World&lt;/h1&gt;
+ * </pre>
+ */
 class Report
 {
 
@@ -17,6 +46,9 @@ class Report
      * @var Report\Config
      */
     private $config;
+    /**
+     * @var StyleSheet[]
+     */
     private $styleSheets = [];
 
     public function __construct()
@@ -24,12 +56,19 @@ class Report
         $this->config = new Report\Config();
     }
 
+    /**
+     * Shorthand way of creating a new report.
+     *
+     * @return \static
+     */
     public static function create()
     {
         return new static();
     }
 
     /**
+     * Get the configuration object for this report.
+     *
      * @return Report\Config
      */
     public function config()
@@ -37,25 +76,49 @@ class Report
         return $this->config;
     }
 
+    /**
+     * Add a StyleSheet object to this report.
+     *
+     * These style sheet objects are added in the head
+     * of the html document.
+     *
+     * @param StyleSheet $styleSheet
+     * @return \PHRE\Report
+     */
     public function addStyleSheet(StyleSheet $styleSheet)
     {
         $this->styleSheets[] = $styleSheet;
         return $this;
     }
 
-    public function generateHTML(DataSource $data)
+    /**
+     * Generate the report and return it's HTML content.
+     * @param mixed $data
+     * @return string The generated report HTML.
+     */
+    public function generateHTML($data)
     {
         return $this->generate(self::FORMAT_HTML, $data);
     }
 
-    public function generatePDF(DataSource $data)
+    /**
+     * Generate the report and return it's raw PDF content.
+     * @param type $data
+     * @return string The generated report PDF.
+     */
+    public function generatePDF($data)
     {
         return $this->generate(self::FORMAT_PDF, $data);
     }
 
-    private function generate($format, DataSource $data)
+    /**
+     * @todo Cleanup.
+     * @todo Support debugging better - config option.
+     */
+    private function generate($format, $data)
     {
-        $data->reset();
+        $dataSource = new DataSource($data);
+
         $this->reset();
 
         ob_start();
@@ -78,10 +141,10 @@ class Report
             // include PDF specific html
         }
 
-        while ($data->current()->valid() and count($parts) < 1000) {
-            $parts[] = $this->renderElements($data);
+        while ($dataSource->current()->valid() and count($parts) < 1000) {
+            $parts[] = $this->renderElements($dataSource);
 
-            $data->next();
+            $dataSource->next();
         }
 
         $parts[] = '</body></html>';
@@ -124,6 +187,10 @@ class Report
         return $styleSheet;
     }
 
+    /**
+     * @todo Allow the user to specify the path to the binary.
+     * @todo Test on all OS's.
+     */
     private function outputPDF($html)
     {
         $margins = $this->config->get(Report\Config::KEY_MARGIN);
--- src/Report/Config.php
+++ src/Report/Config.php
@@ -2,6 +2,19 @@
 
 namespace PHRE\Report;
 
+/**
+ * Class for configuring the options of the report.
+ *
+ * All features of the report which can be configured
+ * are available as public methods. When generating,
+ * the report will read apply these configuration options.
+ *
+ * <b>Defaults:</b>
+ * <ul>
+ *     <li>Orientation: Portrait</li>
+ *     <li>Margins: 10, 10, 10, 10</li>
+ * </ul>
+ */
 class Config
 {
 
@@ -16,6 +29,62 @@ class Config
     {
         $this->setOrientationPortrait();
         $this->setMargin(10, 10, 10, 10);
+    }
+
+    /**
+     * Set report page orientation to Landscape.
+     *
+     * @return Config
+     */
+    public function setOrientationLandscape()
+    {
+        return $this->set(self::KEY_ORIENTATION, self::ORIENTATION_LANDSCAPE);
+    }
+
+    /**
+     * Set report page orientation to Portait.
+     *
+     * @return Config
+     */
+    public function setOrientationPortrait()
+    {
+        return $this->set(self::KEY_ORIENTATION, self::ORIENTATION_PORTRAIT);
+    }
+
+    /**
+     * Set the margin for all the pages.
+     *
+     * All measurements are in milimetres.
+     *
+     * @param double $top Height at the top of the page.
+     * @param double $right Width at the right of the page.
+     * @param double $bottom Height at the bottom of the page.
+     * @param double $left Width at the left of the page.
+     * @return Config
+     */
+    public function setMargin($top, $right, $bottom, $left)
+    {
+        return $this->set(self::KEY_MARGIN, [
+                'Top' => $top,
+                'Right' => $right,
+                'Bottom' => $bottom,
+                'Left' => $left
+        ]);
+    }
+
+    /**
+     * Function to get the config option values.
+     *
+     * @param string $key The key of the stored value. Can be referened by the constants on this class prefixed with <code>KEY_*</pre>.
+     * @return mixed
+     */
+    public function get($key)
+    {
+        if (!array_key_exists($key, $this->config)) {
+            return null;
+        }
+
+        return $this->config[$key];
     }
 
     private function set($key, $value)
@@ -36,35 +105,6 @@ class Config
             $this->config[$key][] = $value;
         }
         return $this;
-    }
-
-    public function setOrientationLandscape()
-    {
-        return $this->set(self::KEY_ORIENTATION, self::ORIENTATION_LANDSCAPE);
-    }
-
-    public function setOrientationPortrait()
-    {
-        return $this->set(self::KEY_ORIENTATION, self::ORIENTATION_PORTRAIT);
-    }
-
-    public function setMargin($top, $right, $bottom, $left)
-    {
-        return $this->set(self::KEY_MARGIN, [
-                'Top' => $top,
-                'Right' => $right,
-                'Bottom' => $bottom,
-                'Left' => $left
-        ]);
-    }
-
-    public function get($key)
-    {
-        if (!array_key_exists($key, $this->config)) {
-            return null;
-        }
-
-        return $this->config[$key];
     }
 
 }
